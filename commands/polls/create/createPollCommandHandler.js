30e48cf3a707c7dc99f6bf928c7b870ade950a6b
sbzhechev <sbzhechev@gmail.com>
2026-02-13 00:48:51 +0200

    Added logic for handling ties in polls.

--- commands/polls/create/createPollCommandHandler.js
+++ commands/polls/create/createPollCommandHandler.js
@@ -57,58 +57,110 @@ const parseOptions = (options) => {
   return optionsValues;
 }
 
-const handlePollResults = (channelId, messageId, duration) => {
+const handlePollResults = (channelId, messageId, duration, isTiebreaker = false) => {
   const durationInMiliseconds = duration * 60 * 60 * 1000 + 1000;
   setTimeout(async () => {
     const getPollResponse = await getPollMessage(channelId, messageId);
     const messageData = await getPollResponse.json();
+
     const pollResults = messageData.poll.results.answer_counts;
     const pollOptions = messageData.poll.answers;
+    const extendedResults = pollOptions.map(option => {
+      const result = pollResults.find(pollResult => pollResult.id === option.answer_id);
+      option.count = result ? result.count : 0;
+      return option;
+    });
     const movies = parseSuggestionsFile();
 
-    const totalVotes = pollResults.reduce((sum, result) => sum += result.count, 0);
-    let winner = null;
-    const skippedMovies = [];
-
-    pollOptions.forEach(option => {
-      const pollResult = pollResults.find(result => result.id === option.answer_id);
-      if (pollResult) {
-        const votePercentage = (pollResult.count / totalVotes) * 100;
-        if (votePercentage <= 10) skippedMovies.push(option.poll_media.text);
-        if (!winner || winner.count < pollResult.count) {
-          winner = { ...pollResult, title: option.poll_media.text };
+    const totalVotes = extendedResults.reduce((sum, result) => sum += result.count, 0);
+    if (totalVotes === 0) {
+      let components = [
+        {
+          type: MessageComponentTypes.TEXT_DISPLAY,
+          content: '@everyone Poll discarded because there are no votes! :('
         }
+      ];
+      return createMessage(channelId, components);
+    }
+
+    extendedResults.sort((result1, result2) => result2.count - result1.count);
+    let winners = [extendedResults[0]];
+
+    for (let index = 1; index < extendedResults.length; index++) {
+      if (extendedResults[index].count === winners[0].count) {
+        winners.push(extendedResults[index]);
       } else {
-        skippedMovies.push(option.poll_media.text);
+        break;
       }
+    }
 
-      const movie = movies.find(movie => movie.title === option.poll_media.text);
-      movie.participated = true;
-    });
+    const skippedMovies = [];
+    if (!isTiebreaker) {
+      extendedResults.forEach(result => {
+        if (result.count > 0) {
+          const votePercentage = (result.count / totalVotes) * 100;
+          if (votePercentage <= 10) skippedMovies.push(result.poll_media.text);
+        } else {
+          skippedMovies.push(result.poll_media.text);
+        }
 
-    const winnerIndex = movies.findIndex(movie => movie.title === winner.title);
-    const winnerMovie = movies.splice(winnerIndex, 1)[0];
-    winnerMovie.watched = true;
-    movies.push(winnerMovie);
+        const movie = movies.find(movie => movie.title === result.poll_media.text);
+        movie.participated = true;
+      });
 
-    skippedMovies.forEach(skippedMovieTitle => {
-      const skippedMovieIndex = movies.findIndex(movie => movie.title === skippedMovieTitle);
-      const skippedMovie = movies.splice(skippedMovieIndex, 1)[0];
-      movies.push(skippedMovie);
-    });
+      skippedMovies.forEach(skippedMovieTitle => {
+        const skippedMovieIndex = movies.findIndex(movie => movie.title === skippedMovieTitle);
+        const skippedMovie = movies.splice(skippedMovieIndex, 1)[0];
+        movies.push(skippedMovie);
+      });
+    }
+
+    if (winners.length === 1) {
+      let winner = winners[0];
+      const winnerIndex = movies.findIndex(movie => movie.title === winner.poll_media.text);
+      const winnerMovie = movies.splice(winnerIndex, 1)[0];
+      winnerMovie.watched = true;
+      movies.push(winnerMovie);
+
+      await createResultsMessage(channelId, winner, skippedMovies);
+      console.log('Poll results parsed!');
+    } else {
+      let newOptions = winners.map((winner, index) => {
+        return {
+          answer_id: index,
+          poll_media: winner.poll_media
+        }
+      })
+
+      let pollObject = {
+        question: { text: '[TIE BREAKER!]' },
+        answers: newOptions,
+        duration: 1,
+        allow_multiselect: false
+      };
+
+      await createResultsMessage(channelId, null, skippedMovies);
+
+      const messageResponse = await createPollMessage(channelId, pollObject);
+      const messageData = await messageResponse.json();
+      const newPollMessageId = messageData.id;
+      handlePollResults(channelId, newPollMessageId, pollObject.duration, true);
+    }
 
     updateSuggestionsFile(movies);
-    await createResultsMessage(channelId, winner, skippedMovies);
-    console.log('Poll results parsed!');
   }, durationInMiliseconds);
 
   console.log('Timeout for poll results created!');
 }
 
 const createResultsMessage = (channelId, winner, skippedMovies) => {
+  const getWinnerText = () => `## The winner is: ${winner.poll_media.text}`;
+  const tieText = '## There\'s been a tie! New tie breaker poll is being generated!';
+  const content = winner ? getWinnerText() : tieText;
+
   let moviesList = '';
-  skippedMovies.forEach(movie => moviesList += `- ${movie}\n`)
   if (skippedMovies.length === 0) moviesList = 'No movies are skipped!';
+  else skippedMovies.forEach(movie => moviesList += `- ${movie}\n`);
 
   let components = [
     {
@@ -120,7 +172,7 @@ const createResultsMessage = (channelId, winner, skippedMovies) => {
       components: [
         {
           type: MessageComponentTypes.TEXT_DISPLAY,
-          content: `## The winner is: ${winner.title}`
+          content
         },
         {
           type: MessageComponentTypes.SEPARATOR
