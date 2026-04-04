8a28dcceebdc0ccbfcd8d898eb4350bc72dcebef
sbzhechev <sbzhechev@gmail.com>
2026-04-04 18:02:06 +0300

    Removed debugging logs.

--- app.js
+++ app.js
@@ -21,9 +21,8 @@ const port = 3000;
 app.use(express.raw({ type: 'application/json' }));
 
 app.post('/interactions', async function (req, res) {
-  console.log('body type:', typeof req.body, Buffer.isBuffer(req.body));
   const isValid = await verifyDiscordRequest(req);
-  console.log('isValid:', isValid);
+
   if (!isValid) return res.status(401).send("Invalid signature");
 
   const body = JSON.parse(req.body.toString('utf-8'));
--- verifyUtils.js
+++ verifyUtils.js
@@ -6,9 +6,6 @@ export const verifyDiscordRequest = (req) => {
   const signature = req.headers["x-signature-ed25519"];
   const timestamp = req.headers["x-signature-timestamp"];
 
-  console.log('signature:', signature);
-  console.log('timestamp:', timestamp);
-  console.log('public key:', process.env.PUBLIC_KEY);
   return verifyKey(req.body, signature, timestamp, process.env.PUBLIC_KEY);
 }
 
